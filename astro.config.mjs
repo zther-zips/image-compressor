b2d705ad01c2458e2f34b03a2d7e777325ca9ccd
z_zhang <156159207+zther-zips@users.noreply.github.com>
2026-08-15 16:02:57 +0800

    Update Vercel adapter configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless"; 
 
 export default defineConfig({
-  output: "server",
-  adapter: vercel(), 
+  output: "server", 
+  adapter: vercel({
+    runtime: 'nodejs22.x', 
+  }),
   integrations: [
     tailwind(), 
     icon(),
